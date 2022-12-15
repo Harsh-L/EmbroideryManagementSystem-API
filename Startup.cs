c17ad9d001bfe23198c46186643b99e153026411
Harsh Lad <68841816+Harsh-L@users.noreply.github.com>
2022-12-15 11:23:53 +0530

    created jwtSettings & added jwt service in startup

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 using EmbroidaryManagementSystem.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -7,9 +8,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace EmbroidaryManagementSystem
@@ -28,6 +31,7 @@ namespace EmbroidaryManagementSystem
         {
             services.AddControllers();
             services.AddDbContext<EmbroidaryManagementSystemContext>(options => options.UseSqlServer(Configuration.GetConnectionString("constr")));
+            JwtConfiguration(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +49,42 @@ namespace EmbroidaryManagementSystem
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        private void JwtConfiguration(IServiceCollection services)
+        {
+            jwtSettings jwtSettings = Configuration.GetSection("JWT").Get<jwtSettings>();
+
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.SecurityKey));
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+                ValidateIssuer = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidateAudience = true,
+                ValidAudience = jwtSettings.Audience,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                RequireExpirationTime = true,
+            };
+
+            //Determines JWT Authentication settings
+            services.AddAuthentication(o => { o.DefaultAuthenticateScheme = jwtSettings.DefaultAuthenticateScheme; })
+                .AddJwtBearer(jwtSettings.DefaultAuthenticateScheme, x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.TokenValidationParameters = tokenValidationParameters;
+                });
+
+            //Determines [Authorize] tag settings
+            services.AddAuthorization(options =>
+            {
+                var defaultAuthorizationPolicyBuilder = new AuthorizationPolicyBuilder(jwtSettings.DefaultAuthenticateScheme);
+                defaultAuthorizationPolicyBuilder = defaultAuthorizationPolicyBuilder.RequireAuthenticatedUser();
+                options.DefaultPolicy = defaultAuthorizationPolicyBuilder.Build();
             });
         }
     }
---
+++ jwtSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmbroidaryManagementSystem
+{
+    public class jwtSettings
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string SecurityKey { get; set; }
+        public int ExpiresTime { get; set; }
+        public string DefaultAuthenticateScheme { get; set; }
+    }
+}
